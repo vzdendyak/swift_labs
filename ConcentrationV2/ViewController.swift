7ad826088eea9fab3167cbda033590f192cf67fd
ad1 <ad1@ad1s-Mac-mini.local>
2022-05-17 10:12:32 +0300

    Lab1

---
+++ ConcentrationV2/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ConcentrationV2
+//
+//  Created by ad1 on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips \(flipCount)"
+        }
+    }
+    
+    var emojiJoices: [String] = ["👻","🎃","👻","🎃"]
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            print("cardNumber = \(cardNumber)")
+            flipCard(emojiJoices[cardNumber], sender)
+        }else{
+            print("not exist")
+        }
+    }
+    
+    func flipCard(_ emoji: String, _ button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
